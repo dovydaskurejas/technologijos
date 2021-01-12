f08ea793aaaea6c1149bb88c7ba22dd764025d69
dovydaskurejas <77335910+dovydaskurejas@users.noreply.github.com>
2021-01-12 15:33:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,131 @@
+basic.showString("Bonjour")
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    . . # . .
+    . . # . .
+    # # . # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . # . # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    # . # . #
+    # # # # #
+    . . # . .
+    # # . # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    # # . # #
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    . . # . .
+    # . # . #
+    . # . # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    # # # # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . # # # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    # . # . #
+    . . # . .
+    # # # # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . # . #
+    # # # # #
+    . . # . .
+    # . # . #
+    . # . # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . # . #
+    # # # # #
+    . . # . .
+    # . # . #
+    # # . # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    # # . # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . # . # .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
